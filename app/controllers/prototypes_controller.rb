2631ace108932cd7782093bbea0dd57f6d2600d4
yokadai <76767115+yokadai@users.noreply.github.com>
2021-01-18 12:24:47 +0900

    update失敗時、その場に留まるよう変更

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -33,8 +33,8 @@ class PrototypesController < ApplicationController
   end
 
   def update
-    prototype = Prototype.find(params[:id])
-    if prototype.update(prototype_params)
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
       redirect_to prototype_path
     else
       render :edit
